dbf66d2fc92a575bab01a55ed0666409dfcc8c2b
seba300 <sebastiandomagala98@gmail.com>
2022-04-08 00:41:48 +0200

    Prepare view layout for account profile.

---
+++ Tidea.Web/Pages/Order/TestAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tidea.Web.Pages.Order
+{
+    public class TestAccount : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
