91d28e42b38d8902785d6538dcc9f383a02ae837
seba300 <sebastiandomagala98@gmail.com>
2022-04-09 22:58:15 +0200

    Finish user management page and edit user data page.

--- Tidea.Web/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Tidea.Web/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,12 +9,37 @@ using Tidea.Core.Entities;
 
 namespace Tidea.Web.Areas.Identity.Pages.Account.Manage
 {
+    [Authorize]
     public class ChangePasswordModel : PageModel
     {
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<ChangePasswordModel> _logger;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly UserManager<ApplicationUser> _userManager;
+        [TempData] public string StatusMessage { get; set; }
+        
+        [BindProperty]
+        [Required]
+        public string FirstName { get; set; }
+        [BindProperty]
+        [Required]
+        public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [BindProperty] 
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [BindProperty] 
+        [StringLength(100, ErrorMessage = "Hasło musi mieć conajmniej {2} znaki i nie więcej niż {1} znaków długości",
+            MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        [BindProperty] 
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Hasła muszą być takie same")]
+        public string NewPasswordVerify { get; set; }
+        
         public ChangePasswordModel(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
@@ -24,62 +50,33 @@ namespace Tidea.Web.Areas.Identity.Pages.Account.Manage
             _logger = logger;
         }
 
-        [BindProperty] public InputModel Input { get; set; }
-
-        [TempData] public string StatusMessage { get; set; }
-
         public async Task<IActionResult> OnGetAsync()
         {
             var user = await _userManager.GetUserAsync(User);
-            if (user == null) return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-
-            var hasPassword = await _userManager.HasPasswordAsync(user);
-            if (!hasPassword) return RedirectToPage("./SetPassword");
+            if (user == null) return NotFound($"Nie znaleziono usera o ID '{_userManager.GetUserId(User)}'.");
+            
+            FirstName = user.FirstName;
+            LastName = user.LastName;
 
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid) return Page();
-
             var user = await _userManager.GetUserAsync(User);
-            if (user == null) return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            var result = await _userManager.ChangePasswordAsync(user, CurrentPassword, NewPassword);
 
-            var changePasswordResult =
-                await _userManager.ChangePasswordAsync(user, Input.OldPassword, Input.NewPassword);
-            if (!changePasswordResult.Succeeded)
+            if (!result.Succeeded)
             {
-                foreach (var error in changePasswordResult.Errors)
-                    ModelState.AddModelError(string.Empty, error.Description);
+                ModelState.AddModelError(string.Empty,errorMessage:"Hasło nie zostało pomyślnie zmienione");
                 return Page();
             }
-
             await _signInManager.RefreshSignInAsync(user);
             _logger.LogInformation("User changed their password successfully.");
-            StatusMessage = "Your password has been changed.";
-
-            return RedirectToPage();
-        }
-
-        public class InputModel
-        {
-            [Required]
-            [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
-            public string OldPassword { get; set; }
-
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-                MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "New password")]
-            public string NewPassword { get; set; }
-
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
+            StatusMessage = "Hasło zostało zmienione";
+            TempData.Keep();
+            
+            return RedirectToPage("Index");
         }
     }
 }
